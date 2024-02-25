e0047b1993c84b199d0a742ca98febde1cae10f9
umanggarg754 <umanggarg754@gmail.com>
2024-02-25 19:22:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+DB_CREDS = {
+    "host": "your host",
+    "dbname": "postgres",
+    "user": "postgres",
+    "password": "your pass",
+    "port": 5432,
+    "sslmode": "require"
+}
+
+S3_CREDS = {
+    "S3_PREFIX": "prefix",
+    "AWS_ACCESS_KEY_ID": "your access id ",
+    "AWS_SECRET_ACCESS": "your access "
+}
